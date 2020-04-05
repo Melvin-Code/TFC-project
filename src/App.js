21febf01ff734988aef7a144381fea23731f395e
Melvin Gonzalez <57917756+Melvin-Code@users.noreply.github.com>
2020-04-04 22:11:16 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from "react";
 import NavBar from './components/NavBar';
 import './App.css';
 import Section1 from './components/Section1';
@@ -8,18 +8,32 @@ import Section4 from './components/Section4';
 import Footer from './components/Footer'
 import Submit from './components/Submit';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-        <Section1/>
-        <Submit/>
-        <Section2/>
-        <Section3/>
-        <Section4/>
-        <Footer/>
-    </div>
-  );
+
+class App extends Component {
+  resolution = () => {
+    if (window.screen.width <= 450) {
+      return <Submit/>;
+    } else {
+  
+    }
+  }
+  render() {
+    return (
+      <div className='App'>
+        
+        <NavBar/>
+          <Section1/>
+          {this.resolution()}
+          <Section2/>
+          <Section3/>
+          <Section4/>
+          <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
+  
+    
+ 
